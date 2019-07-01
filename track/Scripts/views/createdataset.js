d9d779a726493160b581ab5d485d380b9ceb4f99
michaelasanchez <turkeymaster77@gmail.com>
2019-06-30 22:34:20 -0400

    Moved JS for create & edit dataset into separate files

---
+++ track/Scripts/views/createdataset.js
@@ -0,0 +1,87 @@
+﻿
+// Add Property Button
+$('#createAddProperty').click(function (e) {
+
+    var clone = $('#create-dataset .form-row.init').last().clone();
+    console.log(clone);
+
+    var rowCount = $('#create-dataset .form-props > .form-row').length;
+    if (rowCount < maxProperties) {
+
+        // Increment clone input ids
+        var lastId = $('[id^=label]').attr('id');
+        var id = lastId.substr(lastId.length - 1);
+        var nextId = parseInt(id) + 1;
+        $('[id^=label]', clone).attr('id', 'label-' + nextId);
+        $('[id^=label]', clone).val('');
+
+        lastId = $('[id^=type]').attr('id');
+        id = lastId.substr(lastId.length - 1);
+        nextId = parseInt(id) + 1;
+        $('[id^=type]', clone).attr('id', 'type-' + nextId);
+
+        // Button - Remove Row
+        $('button.remove', clone).click(function (e) {
+            // TODO:  improve this
+            if ($(e.target).parent().parent().hasClass('form-row')) {
+                $(e.target).parent().parent().remove();
+            } else if ($(e.target).parent().parent().parent().hasClass('form-row')) {
+                $(e.target).parent().parent().parent().remove();
+            }
+            if ($('#createAddProperty').css('display') == 'none')
+                $('#createAddProperty').show();
+        });
+
+        $('#create-dataset .form-props').append(clone);
+
+        // Highlight first empty label
+        $('[id^=label]').each(function () {
+            if ($(this).val().length == 0) {
+                $(this).focus();
+                return false;
+            }
+        })
+
+        if (rowCount + 1 >= maxProperties) {
+            $('#createAddProperty').hide();
+        }
+    }
+});
+
+
+$('#save-tab').on('click', function () {
+    createDataset();
+})
+
+function createDataset() {
+
+    var datasetLabel = $('#createDatasetLabel').val();
+
+    var propLabels = [];
+    var propTypes = [];
+
+    $.each($('input[id^=label]'), function (index, value) {
+        propLabels.push($(value).val());
+    });
+    $.each($('select[id^=type]'), function (index, value) {
+        propTypes.push($(value).val());
+    });
+    console.log(propTypes);
+
+    var data = {
+        datasetLabel: datasetLabel,
+        labels: propLabels,
+        types: propTypes
+    };
+
+    $.ajax({
+        type: 'POST',
+        url: '/Home/CreateDataset',
+        traditional: true,
+        data: data
+    })
+        .done(function (id) {
+            // Update select and focus new value
+            refreshDatasetOptions($('#datasetSelect'), id);
+        })
+}
---
+++ track/Scripts/views/editdataset.js
@@ -0,0 +1,107 @@
+﻿
+var rowCount = $('#editDataset .form-props > .form-row').length;
+
+$('#editDataset .form-props .form-row:not(.init)').remove();
+$('#editDataset .form-props .form-row.init').attr('id', 'prop-' + currentDataset.ids[0]);
+$('#editDataset #edit-color-0').val(currentDataset.colors[0]);
+$('#editDataset #color-0').css('background-color', '#' + currentDataset.colors[0]);
+
+// Create property rows
+for (var i = 1; i < currentDataset.series.length; i++) {
+    clone = $('#editDataset .form-row.init').last().clone();
+    clone.removeClass('init');
+
+    // Increment clone input ids
+    $(clone).attr('id', 'prop-' + currentDataset.ids[i]);
+    $('[id^=edit-label]', clone).attr('id', 'edit-label-' + i);
+    $('[id^=edit-type]', clone).attr('id', 'edit-type-' + i);
+    $('[id^=edit-color]', clone).attr('id', 'edit-color-' + i);
+    $('[id^=edit-color]', clone).val(currentDataset.colors[i]);
+    $('[id^=color]', clone).attr('id', 'color-' + i);
+    $('[id^=color]', clone).css('background-color', '#' + currentDataset.colors[i]);
+
+    $('#editDataset .form-props').append(clone);
+
+    if (rowCount + 1 == maxProperties) {
+        $('#editAddProperty').hide();
+    }
+}
+
+// Populate values
+$('#edit-datasetLabel').val(currentDataset.label);
+for (var i = 0; i < currentDataset.series.length; i++) {
+    $('#editDataset #edit-label-' + i).val(currentDataset.series[i]);
+    $('#editDataset #edit-type-' + i + ' option[value=' + currentDataset.types[i] + ']').attr('selected', true);
+}
+
+// Input change events
+$('[id^=edit-color]').change(function (e) {
+
+    var id = $(e.target).attr('id');
+
+
+    $('#color-' + id.substr(id.length - 1)).css('background-color', '#' + $(e.target).val());
+});
+$('#edit-datasetLabel').change(function (e) {
+    $(e.target).addClass('is-valid');
+});
+$('[id^=edit-label]').change(function (e) {
+    $(e.target).addClass('is-valid');
+});
+$('[id^=edit-color]').change(function (e) {
+    $(e.target).addClass('is-valid');
+});
+
+$('#save-tab').on('click', function () {
+    editDataset();
+})
+
+function editDataset() {
+
+    var datasetLabel;
+
+    if ($('#edit-datasetLabel').hasClass('is-valid'))
+        datasetLabel = $('#edit-datasetLabel').val();
+
+    var propIds = [];
+    var propLabels = []
+    var propColors = [];
+
+    $.each($('#editDataset .form-props .form-row'), function (index, value) {
+
+        if ($('#edit-label-' + index, value).hasClass('is-valid') || $('#edit-color-' + index).hasClass('is-valid')) {
+            var propId = $(value).attr('id');
+            propIds.push(propId.substr(propId.indexOf('-') + 1));
+            propLabels.push($('#edit-label-' + index, value).val());
+            propColors.push($('#edit-color-' + index, value).val());
+        }
+    });
+
+    var data = {
+        datasetId: $('#datasetSelect').val(),
+        datasetLabel: datasetLabel,
+        ids: propIds,
+        labels: propLabels,
+        colors: propColors
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: '/Home/UpdateDataset',
+        traditional: true,
+        data: data
+    })
+        .done(function () {
+            $.get('/Home/GetDataset/' + $('#datasetSelect').val(), function (data) {
+                currentDataset = JSON.parse(data);
+                refreshForm(currentDataset);
+                refreshChart(currentDataset, $('.ct-chart'));
+
+                // Update Dataset Select
+                $('#datasetSelect option:selected').html(currentDataset.label);
+
+                // Reset Edit Dataset Form
+                $('#editDataset .is-valid').removeClass('is-valid');
+            });
+        });
+}
