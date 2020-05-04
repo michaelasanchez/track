7bc9e32ff600eb2d1bf39e29eaa80e52cf15ed12
michaelasanchez <turkeymaster77@gmail.com>
2020-05-04 00:14:10 -0400

    Missed utils

---
+++ track-api/Utils/UserUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using track_api.Models;
+
+namespace track_api.Utils
+{
+    public static class UserUtils
+    {
+        public static User GetUser(TrackContext context, HttpContext httpContext)
+        {
+            var userClaims = httpContext.GetOwinContext().Authentication.User.Claims;
+
+            if (userClaims.Any())
+            {
+                var clientId = userClaims.FirstOrDefault(z => z.Type == "cid").Value;
+                var userId = userClaims.FirstOrDefault(z => z.Type == "uid")?.Value;
+
+                return context.Users.FirstOrDefault(z => z.OktaUserId == userId);
+            }
+
+            return null;
+        }
+    }
+}
