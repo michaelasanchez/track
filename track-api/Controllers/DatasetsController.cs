97954f5381de8c892541b0b84cf188dde469570e
michaelasanchez <turkeymaster77@gmail.com>
2020-06-08 23:06:19 -0400

    Delete dataset should only archive

--- track-api/Controllers/DatasetsController.cs
+++ track-api/Controllers/DatasetsController.cs
@@ -178,7 +178,7 @@ namespace track_api.Controllers
                 return NotFound();
             }
 
-            db.Datasets.Remove(dataset);
+            dataset.Archived = true;
             db.SaveChanges();
 
             return StatusCode(HttpStatusCode.NoContent);
--- track-web/src/components/Toolbar.tsx
+++ track-web/src/components/Toolbar.tsx
@@ -70,6 +70,7 @@ const Toolbar: React.FunctionComponent<ToolbarProps> = ({
     return (
       <Form.Control
         as="select"
+        className="custom-select"
         disabled={mode == UserMode.Edit || !hasDatasets}
         onChange={(e: React.FormEvent) => updateDataset($(e.target).val())}
         value={dataset?.Id.toString()}>
